03fa5b15b4fbe6c8363a2611728f07994d927943
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-14 16:40:48 +0100

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,45 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#First task
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, i|
+    puts "#{i + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#Second task
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|planeteer_call| "#{planeteer_call}!".capitalize}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+#Or
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect  {|planeteer_call| "#{planeteer_call<<"!"}".capitalize}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+
+#Third task
+def long_planeteer_calls(calls)
+  i = 0
+  if calls.all? {|i| i.length <= 4}
+  false
+  elsif calls.any? {|i| i.length > 4}
+  true
+  else
+  false
+  i =+ 1
+end
+end
+
+
+#Fourth task
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|i| cheese_types.include?(i)}
+end
+
+#Or
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|type| cheese_types.include?(type)}
 end
